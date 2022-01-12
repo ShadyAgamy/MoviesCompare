8836c65c23cc148b613857bd9b636d8e02e23a99
shadyagmy <72800568+ShadyAgamy@users.noreply.github.com>
2022-01-12 15:56:25 +0200

    before compare between two movies

--- createAutoComplete.js
+++ createAutoComplete.js
@@ -1,59 +1,30 @@
-const posterPathUrl = "https://image.tmdb.org/t/p/w185/";
+const createAutoComplete = ({ root, createOption, onOptionSelect,fetchData }) => {
+  const input = root.querySelector("input");
+  const options = root.querySelector(".input_options");
+
+  // fetch data func
+  const onInput = async (e) => {
+    document.querySelector('.tutorial').classList.add('close')
+    options.innerHTML = "";
+    const items = await fetchData(e.target.value);
+    options.classList.remove("close");
+    items.length === 0
+      ? options.classList.add("close")
+      : items.forEach((movie) => {
+          let media = createOption(movie);
+          options.appendChild(media);
+          media.addEventListener("click", () => onOptionSelect(movie,input,options));
+        });
+  };
 
 
-// create option func
-const createOption = (movie) => {
-    const {poster_path, title} = movie
-  const media = document.createElement("div");
-  media.classList.add("media");
-  const img = poster_path ? `<img src=${posterPathUrl + poster_path}>` : "";
-  media.innerHTML = `<figure class="media-left">
-    <p class="image is-64x64">
-    ${img}
-    </p>
-  </figure>
-  <div class="media-content">
-    <div class="content">
-        <p class="movieTitle">${title} </p> 
-    </div>
-  </div>`;
-  return media;
-};
-
-// fetch data func
-const fetchData = async (queryName, input_options, input) => {
-  const response = await axios
-    .get(url, {
-      params: {
-        query: queryName,
-      },
-    })
-    .then(
-      (response) => {
-        const data = response.data.results.slice(0, 10);
-        showData(data, input_options, input);
-      },
-      (error) => {
-        input_options.classList.add("close");
+  document.addEventListener("click", (event) => {
+    if (!root.contains(event.target)) {
+      if (options.childNodes.length > 0) {
+        options.classList.add("close");
       }
-    );
-};
+    }
+  });
 
-// show data 
-const showData = (data, options, input) => {
-  options.classList.remove("close");
-  data.length === 0
-    ? options.classList.add("close")
-    : data.forEach((movie) => {
-        let media = createOption(movie);
-        options.appendChild(media);
-        media.addEventListener("click", () => getMovie(movie, options, input));
-      });
-};
-
-
-const getMovies = (e, options, input) => {
-  const movieName = e.target.value;
-  options.innerHTML = "";
-  fetchData(movieName, options, input);
+  input.addEventListener("input", debounce(onInput, 500));
 };
--- index.js
+++ index.js
@@ -1,14 +1,134 @@
 const API_KEY = "7bbee3bd234e91723f59f57f5ef6b033";
-const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}`;
-const selectMovieInput1 = document.getElementById("selectMovieInput1");
-const selectMovieInput2 = document.getElementById("selectMovieInput2");
-const column = document.querySelector(".column");
-let input_options1 = document.getElementById("input_options1");
-let input_options2 = document.getElementById("input_options2");
 
-const detailsCard = document.createElement("div");
-detailsCard.classList.add("card", "movieDetails");
+const autoCompleteConfig = {
+  createOption: (movie) => {
+    const posterPathUrl = "https://image.tmdb.org/t/p/w185/";
+    const { poster_path, title } = movie;
+    const media = document.createElement("div");
+    media.classList.add("media");
+    const img = poster_path ? `<img src=${posterPathUrl + poster_path}>` : "";
+    media.innerHTML = `<figure class="media-left">
+            <p class="image is-64x64">
+            ${img}
+            </p>
+          </figure>
+          <div class="media-content">
+            <div class="content">
+                <p class="movieTitle">${title} </p> 
+            </div>
+          </div>`;
+    return media;
+  },
 
+  async fetchData(queryName) {
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}`;
+    const response = await axios.get(url, {
+      params: {
+        query: queryName,
+      },
+    });
+    if (response.data.results.length === 0) {
+      return [];
+    }
+    return response.data.results.slice(0, 10);
+  },
+};
+
+createAutoComplete({
+  ...autoCompleteConfig,
+  root: document.querySelector(".left-auto-complete"),
+  onOptionSelect: (movie) => {
+    getMovie(movie, document.querySelector(".left-auto-complete"), "left");
+  },
+});
+createAutoComplete({
+  ...autoCompleteConfig,
+  root: document.querySelector(".right-auto-complete"),
+  onOptionSelect: (movie) => {
+    getMovie(movie, document.querySelector(".right-auto-complete"), "right");
+  },
+});
+
+let leftMovie;
+let rightMovie;
+const getMovie = async (movie, root, side) => {
+  const input = root.querySelector("input");
+  const options = root.querySelector(".input_options");
+  input.value = movie.title;
+  options.classList.add("close");
+  const movieDetails = await fetchMovieRequest(movie.id);
+  showMovieDetails(movieDetails, root);
+  side === "left" ? (leftMovie = movie) : (rightMovie = movie);
+  if (leftMovie && rightMovie) {
+    runComparison();
+  }
+};
+
+const runComparison = () => {
+  console.log(leftMovie);
+  console.log(rightMovie);
+  if (leftMovie.vote_average > rightMovie.vote_average) {
+    const notification = document
+      .querySelector(".left-auto-complete")
+      .querySelector(".vote_average");
+      console.log(notification)
+      notification.classList.add('is-primary')
+  } else if (leftMovie.vote_average === rightMovie.vote_average) {
+    console.log("equel");
+  } else {
+    console.log("right");
+  }
+};
+
+const fetchMovieRequest = async (id) => {
+  const response = await axios.get(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`,
+    {}
+  );
+  return response.data;
+};
+
+const showMovieDetails = (movie, root) => {
+  const posterPathUrl = "https://image.tmdb.org/t/p/w185/";
+  const detailsCard = root.querySelector(".card");
+  const { poster_path, title, genres, id, revenue, popularity, vote_average } =
+    movie;
+  const renderGeners = showGeneres(genres);
+  detailsCard.innerHTML = "";
+  detailsCard.innerHTML = `
+    <div class="card-image">
+      <figure class="image is-4by3">
+        <img style="object-fit:contain;" src="${
+          posterPathUrl + poster_path
+        }" alt="Movie Poster">
+      </figure>
+    </div>
+    <div class="card-content">
+      <div class="media">
+        <div class="media-content">
+          <p class="title is-4">${title}</p>
+          <p class="subtitle ${"generes" + id} is-6"></p>
+        </div>
+      </div>
+      <article class="notification is-info">
+          <p class="title">${usdCurrencyFormatter.format(parseInt(revenue))}</p>
+          <p class="subtitle">Awards</p>
+      </article>
+      <article class="notification is-info">
+          <p class="title">${popularity}</p>
+          <p class="subtitle">popularity</p>
+      </article>
+      <article class="notification is-info vote_average">
+          <p class="title">${vote_average}</p>
+          <p class="subtitle">vote</p>
+      </article>
+      
+    </div>
+      `;
+  root.append(detailsCard);
+  const generesDiv = document.querySelector(`.generes${id}`);
+  generesDiv.append(renderGeners);
+};
 const showGeneres = (generes) => {
   const ul = document.createElement("ul");
   ul.classList.add("generes-list");
@@ -18,82 +138,4 @@ const showGeneres = (generes) => {
     ul.append(li);
   });
   return ul;
-};
-
-const showMovieDetails = (movie) => {
-  const { poster_path, title, genres, id, revenue, popularity, vote_average } =
-    movie;
-  const renderGeners = showGeneres(genres);
-  detailsCard.innerHTML = "";
-  detailsCard.innerHTML = `
-  <div class="card-image">
-    <figure class="image is-4by3">
-      <img style="object-fit:contain;" src="${
-        posterPathUrl + poster_path
-      }" alt="Movie Poster">
-    </figure>
-  </div>
-  <div class="card-content">
-    <div class="media">
-      <div class="media-content">
-        <p class="title is-4">${title}</p>
-        <p class="subtitle ${"generes" + id} is-6"></p>
-      </div>
-    </div>
-    <article class="notification is-primary">
-        <p class="title">${usdCurrencyFormatter.format(parseInt(revenue))}</p>
-        <p class="subtitle">Awards</p>
-    </article>
-    <article class="notification is-primary">
-        <p class="title">${popularity}</p>
-        <p class="subtitle">popularity</p>
-    </article>
-    <article class="notification is-primary">
-        <p class="title">${vote_average}</p>
-        <p class="subtitle">vote</p>
-    </article>
-    
-  </div>
-    `;
-  column.append(detailsCard);
-  const generesDiv = document.querySelector(`.generes${id}`);
-  generesDiv.append(renderGeners);
-};
-
-selectMovieInput1.addEventListener(
-  "input",
-  debounce((e) => getMovies(e, input_options1, selectMovieInput1), 500)
-);
-selectMovieInput2.addEventListener(
-  "input",
-  debounce((e) => getMovies(e, input_options2, selectMovieInput2), 500)
-);
-
-
-
-
-document.addEventListener("click", (event) => {
-  // for (let i = 0; i < input_containers.length; i++) {
-  if (!column.contains(event.target)) {
-    if (input_options1.childNodes.length > 0) {
-      input_options1.classList.add("close");
-    }
-  }
-  // }
-});
-
-const getMovie = async (movie, input_options, input) => {
-    console.log(input)
-    input.value = movie.title;
-  input_options.classList.add("close");
-  const movieDetails = await fetchMovieRequest(movie.id);
-  showMovieDetails(movieDetails);
-};
-
-const fetchMovieRequest = async (id) => {
-  const response = await axios.get(
-    `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`,
-    {}
-  );
-  return response.data;
 };
