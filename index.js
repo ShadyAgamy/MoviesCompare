ed4d95fa6cf917855244ff2080b08f68a4f3b29c
shadyagmy <72800568+ShadyAgamy@users.noreply.github.com>
2022-01-13 01:53:18 +0200

    finish the app and adding some styling

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const API_KEY = "7bbee3bd234e91723f59f57f5ef6b033";
+const API_KEY = '7bbee3bd234e91723f59f57f5ef6b033';
 
 const autoCompleteConfig = {
   createOption: (movie) => {
-    const posterPathUrl = "https://image.tmdb.org/t/p/w185/";
+    const posterPathUrl = 'https://image.tmdb.org/t/p/w185/';
     const { poster_path, title } = movie;
-    const media = document.createElement("div");
-    media.classList.add("media");
-    const img = poster_path ? `<img src=${posterPathUrl + poster_path}>` : "";
+    const media = document.createElement('div');
+    media.classList.add('media');
+    const img = poster_path ? `<img src=${posterPathUrl + poster_path}>` : '';
     media.innerHTML = `<figure class="media-left">
             <p class="image is-64x64">
             ${img}
@@ -36,89 +36,92 @@ const autoCompleteConfig = {
 
 createAutoComplete({
   ...autoCompleteConfig,
-  root: document.querySelector(".left-auto-complete"),
+  root: document.querySelector('.left-auto-complete'),
   onOptionSelect: (movie) => {
-    getMovie(movie, document.querySelector(".left-auto-complete"), "left");
+    getMovie(movie, document.querySelector('.left-auto-complete'), 'left');
   },
 });
 createAutoComplete({
   ...autoCompleteConfig,
-  root: document.querySelector(".right-auto-complete"),
+  root: document.querySelector('.right-auto-complete'),
   onOptionSelect: (movie) => {
-    getMovie(movie, document.querySelector(".right-auto-complete"), "right");
+    getMovie(movie, document.querySelector('.right-auto-complete'), 'right');
   },
 });
 
 let leftMovie;
 let rightMovie;
 const getMovie = async (movie, root, side) => {
-  const input = root.querySelector("input");
-  const options = root.querySelector(".input_options");
+  const input = root.querySelector('input');
+  const options = root.querySelector('.input_options');
   input.value = movie.title;
-  options.classList.add("close");
+  options.classList.add('close');
   const movieDetails = await fetchMovieRequest(movie.id);
   showMovieDetails(movieDetails, root);
-  side === "left" ? (leftMovie = movie) : (rightMovie = movie);
+  side === 'left' ? (leftMovie = movie) : (rightMovie = movie);
   if (leftMovie && rightMovie) {
     runComparison();
   }
 };
 
 const runComparison = () => {
-  console.log(leftMovie);
-  console.log(rightMovie);
-  if (leftMovie.vote_average > rightMovie.vote_average) {
-    const notification = document
-      .querySelector(".left-auto-complete")
-      .querySelector(".vote_average");
-      console.log(notification)
-      notification.classList.add('is-primary')
-  } else if (leftMovie.vote_average === rightMovie.vote_average) {
-    console.log("equel");
-  } else {
-    console.log("right");
-  }
+  const leftSideStats = document.querySelectorAll('.left-auto-complete .notification');
+  const rightSideStats = document.querySelectorAll('.right-auto-complete .notification');
+
+  leftSideStats.forEach((leftStat, i) => {
+    const rightStat = rightSideStats[i];
+    const leftStatValue = leftStat.dataset.value;
+    const rightStatValue = rightStat.dataset.value;
+
+    if (leftStatValue > rightStatValue) {
+      leftStat.classList.remove('is-info');
+      leftStat.classList.add('is-primary');
+      rightStat.classList.add('is-info');
+      rightStat.classList.remove('is-primary');
+    }
+    if (rightStatValue > leftStatValue) {
+      rightStat.classList.remove('is-info');
+      rightStat.classList.add('is-primary');
+      leftStat.classList.add('is-info');
+      leftStat.classList.remove('is-primary');
+    }
+  });
 };
 
 const fetchMovieRequest = async (id) => {
-  const response = await axios.get(
-    `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`,
-    {}
-  );
+  const response = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`, {});
   return response.data;
 };
 
 const showMovieDetails = (movie, root) => {
-  const posterPathUrl = "https://image.tmdb.org/t/p/w185/";
-  const detailsCard = root.querySelector(".card");
-  const { poster_path, title, genres, id, revenue, popularity, vote_average } =
-    movie;
+  const posterPathUrl = 'https://image.tmdb.org/t/p/w185/';
+  const detailsCard = root.querySelector('.card');
+  const { poster_path, title, genres, id, revenue, popularity, vote_average } = movie;
+  const revenueNumber = revenue;
   const renderGeners = showGeneres(genres);
-  detailsCard.innerHTML = "";
+  detailsCard.innerHTML = '';
   detailsCard.innerHTML = `
     <div class="card-image">
       <figure class="image is-4by3">
-        <img style="object-fit:contain;" src="${
-          posterPathUrl + poster_path
-        }" alt="Movie Poster">
+        <img style="object-fit:contain;" src="${posterPathUrl + poster_path}" alt="Movie Poster">
       </figure>
     </div>
     <div class="card-content">
       <div class="media">
         <div class="media-content">
           <p class="title is-4">${title}</p>
-          <p class="subtitle ${"generes" + id} is-6"></p>
+          <p class="subtitle ${'generes' + id} is-6"></p>
         </div>
       </div>
-      <article class="notification is-info">
+      <article data-value=${revenue} class="notification is-info">
           <p class="title">${usdCurrencyFormatter.format(parseInt(revenue))}</p>
-          <p class="subtitle">Awards</p>
+          <p class="subtitle">revenue</p>
       </article>
-      <article class="notification is-info">
+      <article data-value=${popularity} class="notification is-info">
           <p class="title">${popularity}</p>
           <p class="subtitle">popularity</p>
       </article>
-      <article class="notification is-info vote_average">
+      <article data-value=${vote_average} class="notification is-info vote_average">
           <p class="title">${vote_average}</p>
           <p class="subtitle">vote</p>
       </article>
@@ -130,10 +133,10 @@ const showMovieDetails = (movie, root) => {
   generesDiv.append(renderGeners);
 };
 const showGeneres = (generes) => {
-  const ul = document.createElement("ul");
-  ul.classList.add("generes-list");
+  const ul = document.createElement('ul');
+  ul.classList.add('generes-list');
   generes.forEach((genere) => {
-    const li = document.createElement("li");
+    const li = document.createElement('li');
     li.innerHTML = genere.name;
     ul.append(li);
   });
--- utils.js
+++ utils.js
@@ -14,30 +14,3 @@ const usdCurrencyFormatter = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
 });
-
-
-// adult: false
-// backdrop_path: "/nNmJRkg8wWnRmzQDe2FwKbPIsJV.jpg"
-// belongs_to_collection: {id: 86311, name: 'The Avengers Collection', poster_path: '/yFSIUVTCvgYrpalUktulvk3Gi5Y.jpg', backdrop_path: '/zuW6fOiusv4X9nnW3paHGfXcSll.jpg'}
-// budget: 220000000
-// genres: (3) [{…}, {…}, {…}]
-// homepage: "http://marvel.com/avengers_movie/"
-// id: 24428
-// imdb_id: "tt0848228"
-// original_language: "en"
-// original_title: "The Avengers"
-// overview: "When an unexpected enemy emerges and threatens global safety and security, Nick Fury, director of the international peacekeeping agency known as S.H.I.E.L.D., finds himself in need of a team to pull the world back from the brink of disaster. Spanning the globe, a daring recruitment effort begins!"
-// popularity: 461.005
-// poster_path: "/RYMX2wcKCBAr24UyPD7xwmjaTn.jpg"
-// production_companies: [{…}]
-// production_countries: [{…}]
-// release_date: "2012-04-25"
-// revenue: 1518815515
-// runtime: 143
-// spoken_languages: (3) [{…}, {…}, {…}]
-// status: "Released"
-// tagline: "Some assembly required."
-// title: "The Avengers"
-// video: false
-// vote_average: 7.7
-// vote_count: 26119
